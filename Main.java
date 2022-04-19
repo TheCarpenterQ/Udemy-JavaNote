e1502e63fb01c25366825f3c763b8552392b3983
TheCarpenterQ <82557598+TheCarpenterQ@users.noreply.github.com>
2022-04-19 17:15:15 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';  //16bits;for single character
+        char myUnicodeChar = '\u0044';  //0044:'D'in unicode
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
