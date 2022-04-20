80d56513d08c7f3bd97b30b00379fd07544f4c0e
TheCarpenterQ <82557598+TheCarpenterQ@users.noreply.github.com>
2022-04-20 14:21:29 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //int int2 = 5;
+        //false,true,null
+
+        //a mile is equal to 1.609344 km
+        double kilometers = (100 * 1.609344);
+        int highscore = 50;
+        if (highscore == 50){
+            System.out.println("This is an expression");
+        }
+    }
+}
