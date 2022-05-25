e6da9c53cbcbb7afe2219504545e81f4cceb3fc9
TheCarpenterQ <82557598+TheCarpenterQ@users.noreply.github.com>
2022-05-26 01:22:52 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+//        Gearbox mcLaren = new Gearbox(6);
+//
+//        mcLaren.operaterClutch(true);
+//        mcLaren.changeGear(1);
+//        mcLaren.operaterClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(1000));
+//        mcLaren.changeGear(2);
+//        System.out.println(mcLaren.wheelSpeed(3000));
+//        mcLaren.operaterClutch(true);
+//        mcLaren.changeGear(3);
+//        mcLaren.operaterClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(6000));
+
+
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener(){
+//                System.out.println("I've been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//
+//        btnPrint.setOnClickListener(new ClickListener());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            //an anonymous class
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + "was clicked");
+            }
+        });
+        listen();
+    }
+
+    private static void listen(){
+        boolean quit = false;
+        while (!quit){
+            int chioce = scanner.nextInt();
+            scanner.nextLine();
+            switch (chioce){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
+    }
+}
